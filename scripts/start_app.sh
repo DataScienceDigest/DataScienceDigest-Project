7668a16ea82429f0c249e2051b077dbdb1da4e6f
DataScienceDigest <datasciencedigest.in@gmail.com>
2024-09-05 00:32:36 +0530

    new changes

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["13.60.215.191"]/' /home/ubuntu/DataScienceDigest-Project/DataScienceDigest/settings.py
+sed -i 's/\[]/\["16.171.176.57"]/' /home/ubuntu/DataScienceDigest-Project/DataScienceDigest/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
