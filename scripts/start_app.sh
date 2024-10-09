55d5e5da11f289e4f3d21332404dec8fce257b73
DataScienceDigest <datasciencedigest.in@gmail.com>
2024-10-09 22:37:28 +0530

    error solving

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash 
-
+# Change ownership of the project directory
+sudo chown -R ubuntu:ubuntu /home/ubuntu/DataScienceDigest-Project
 sed -i 's/\[]/\["16.171.176.57"]/' /home/ubuntu/DataScienceDigest-Project/DataScienceDigest/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
 python manage.py collectstatic
+sudo certbot --nginx -d datasciencedigest.in -d www.datasciencedigest.in --non-interactive --agree-tos
 sudo service gunicorn restart
 sudo service nginx restart
 #sudo tail -f /var/log/nginx/error.log
